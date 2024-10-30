051848119ad6c29e61c96b4d1dc421f24da07806
Mateus Silva <mateusilva767@gmail.com>
2024-10-29 22:41:08 -0300

    landing page created

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-links");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerview: 1,
+    },
+    768: {
+      slidesPerview: 2,
+    },
+    1024: {
+      slidesPerview: 3,
+    },
+  },
+});
